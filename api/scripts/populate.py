ca60b6ce799dbe1107c1523d15e303f82609bd48
Arthur Soares <arthurpmrs@gmail.com>
2026-04-05 15:04:48 -0300

    fix: improve populate script

--- api/scripts/populate.py
+++ api/scripts/populate.py
@@ -1,16 +1,22 @@
+from sqlalchemy import func, select
 from sqlalchemy.dialects.postgresql import insert
 
 from src.core.db import engine
 from src.features.auth.tables import students
 
 
-def seed():
+def populate():
     stmt = insert(students).values(name='admin', email='admin@admin.com')
     do_nothing_stmt = stmt.on_conflict_do_nothing(index_elements=['id'])
 
     with engine.begin() as conn:
+        count = conn.execute(select(func.count()).select_from(students)).scalar()
+
+        if count is not None and count > 0:
+            return
+
         conn.execute(do_nothing_stmt).fetchone()
 
 
 if __name__ == '__main__':
-    seed()
+    populate()
