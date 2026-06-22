9d2321e368588a283685ebbb29c286745e10bfbf
Arthur Soares <arthurpmrs@gmail.com>
2026-06-22 20:41:11 -0300

    fix: depenate login page

--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,7 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 import Home from '@/features/home';
 
 export const Route = createFileRoute('/')({
+  beforeLoad: () => {
+    throw redirect({
+      to: '/goals',
+      replace: true,
+    })
+  },
   component: Home,
 });
